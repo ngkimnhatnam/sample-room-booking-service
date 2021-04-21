7af6edfad231531a18f83c8df57a83a4d42d0eef
Nam <ngkimnhatnam@gmail.com>
2021-04-21 15:47:40 +0300

    added database configuration

---
+++ src/configs/database.ts
@@ -0,0 +1,11 @@
+import mysql from 'mysql';
+
+const connection = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: Number(process.env.PORT),
+});
+
+export default connection;
