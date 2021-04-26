9ccc199a4098df5705da6dec10d2ace6b6716a20
Nam <ngkimnhatnam@gmail.com>
2021-04-26 15:59:34 +0300

    added Swagger docs

--- src/routes/room.ts
+++ src/routes/room.ts
@@ -72,6 +72,101 @@ const router = express.Router();
  */
 router.get('/rooms', roomController.getRooms);
 
+/**
+ * @swagger
+ * paths:
+ *  /api/v1/rooms/{room_id}:
+ *   get:
+ *     tags:
+ *       - Rooms
+ *     name: Get a specific room
+ *     summary: Get a specific room
+ *     description: "This API is used to retrieve data from a specific room with provided id"
+ *     consumes:
+ *       - application/json
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - in: path
+ *         name: room_id
+ *         description: "Room Id to be identified in the system"
+ *         schema:
+ *           type: int
+ *           example: 5
+ *         required: true
+ *     responses:
+ *       '200':
+ *         description: Success
+ *         schema:
+ *           type: object
+ *           properties:
+ *             message:
+ *               type: string
+ *               example: Room data retrieved successfully
+ *             status:
+ *               type: int
+ *               example: 200
+ *             data:
+ *               type: object
+ *               properties:
+ *                 room_id:
+ *                   type: int
+ *                   example: 1
+ *                 room_name:
+ *                   type: string
+ *                   example: 'Sauna'
+ *                 opening_hour:
+ *                   type: string
+ *                   example: '08:00:00'
+ *                 closing_hour:
+ *                   type: string
+ *                   example: '17:00:00'
+ *                 base_price:
+ *                   type: int
+ *                   example: 1000
+ *                 timezone:
+ *                   type: string
+ *                   example: Europe/Helsinki
+ *                 is_active:
+ *                   type: int
+ *                   example: 1
+ *                 bookings_raw:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       booking_id:
+ *                         type: int
+ *                         example: 1
+ *                       start_time:
+ *                         type: int
+ *                         example: 16032131
+ *                       end_time:
+ *                         type: int
+ *                         example: 16042323
+ *                 bookings_formatted:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       booking_id:
+ *                         type: int
+ *                         example: 1
+ *                       start:
+ *                         type: string
+ *                         example: '26-04-2021 12:00'
+ *                       end:
+ *                         type: string
+ *                         example: '26-04-2021 15:00'
+ *       '500':
+ *         description: Internal error
+ *         schema:
+ *           type: object
+ *             properties:
+ *               message:
+ *                 type: string
+ *                 example: Something went wrong
+ */
 router.get('/rooms/:room_id', roomController.getRoom);
 
 export default router;
--- src/services/room.ts
+++ src/services/room.ts
@@ -29,6 +29,8 @@ export const getAllRooms = async () => {
 export const getOneRoom = async (room_id: number) => {
   try {
     const room = (await roomModel.findOne(room_id)) as FinalRoomData;
+
+    /* Room booking schedule is formatted here according to its timezone */
     room.bookings_formatted = room.bookings_raw.map((booking) => {
       const start = DateTimeConversion.DateTime.fromSeconds(booking.start_time, { zone: room.timezone });
       const end = DateTimeConversion.DateTime.fromSeconds(booking.end_time, { zone: room.timezone });
