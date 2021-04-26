2397dc67e3a22dff512507c4da69fdb4787b4a27
Nam <ngkimnhatnam@gmail.com>
2021-04-26 16:05:14 +0300

    test case added

--- src/tests/room.test.ts
+++ src/tests/room.test.ts
@@ -20,3 +20,20 @@ describe('Testing GET /rooms endpoint', () => {
     done();
   });
 });
+
+describe('Testing GET /rooms/:room_id endpoint', () => {
+  it('Should return data for room 1', async (done) => {
+    const app = await loaders({ expressApp: application });
+    const res = await request(app)
+      .get('/api/v1/rooms/1')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/);
+    expect(res.status).toEqual(200);
+    expect(res.body.message).toEqual('Room data retrieved successfully');
+    expect(res.body.data.room_id).toEqual(1);
+    expect(res.body.data.room_name).toEqual('Sauna');
+    expect(res.body.data.bookings_raw).toBeInstanceOf(Array);
+    expect(res.body.data.bookings_formatted).toBeInstanceOf(Array);
+    done();
+  });
+});
