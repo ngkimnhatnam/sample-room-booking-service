9650c560a5a0268afab5b4743af12635bf2eced3
Nam <ngkimnhatnam@gmail.com>
2021-04-28 11:33:04 +0300

    project documentation update

--- src/configs/database.ts
+++ src/configs/database.ts
@@ -1,6 +1,4 @@
 import mysql from 'mysql';
-import dotenv from 'dotenv';
-dotenv.config();
 
 const connection = mysql.createPool({
   host: process.env.MYSQL_HOST,
--- src/configs/payment.ts
+++ src/configs/payment.ts
@@ -1,6 +1,3 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
 const paymentConfig = {
   stripe_secret_key: process.env.STRIPE_SECRET_KEY!,
   api_version: process.env.API_VERSION!,
